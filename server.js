c3399248f2865abb629107491d3e73e572559849
unknown <aschuprov@gmail.com>
2018-07-19 01:28:19 +0300

    back

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 var express = require('express');
 
 var app = express();
-var router = express.Router();
-var views = __dirname + '/views/';
-
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
-
-router.get("/",function(req,res){
-  res.sendFile(views + "index.html");
-});
-
-app.use("/",router);
-
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/Hello World!');
 });
 
 var port = 8080;
