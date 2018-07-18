7ec2cd6f2beb72ae7a5fc3da7be17d75230713e5
unknown <aschuprov@gmail.com>
2018-07-19 00:36:43 +0300

    Root

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+console.log('Hello from root!');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World from Root\n');
+});
+server.listen(server_port, server_ip_address, () => {
+  console.log(`Server running at http://${server_ip_address}:${server_port}/`);
+});
