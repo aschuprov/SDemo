26722e90a280b687398bbbb22bef8c2613a54ac0
unknown <aschuprov@gmail.com>
2018-07-19 01:23:29 +0300

    use

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ var app = express();
 var router = express.Router();
 var views = __dirname + '/views/';
 
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
 router.get("/",function(req,res){
   res.sendFile(views + "index.html");
 });
 
 app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
 
 var port = 8080;
 var ip   = '0.0.0.0';
