8fbf16472456ad8f335e471b8ba4950b72bccf72
unknown <aschuprov@gmail.com>
2018-07-19 00:42:37 +0300

    ip test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-var server_port = process.env.server_port || process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.server_ip_address || process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1'
 
 console.log('Hello from root!');
 
