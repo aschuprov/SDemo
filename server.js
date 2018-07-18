0b2f9fe334e3fa05807c196cc11ac3da50d84dfe
unknown <aschuprov@gmail.com>
2018-07-19 01:10:30 +0300

    zero ip

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var port = process.env.PORT || process.env.SDEMO_SERVICE_PORT || 8080;
-var ip   = process.env.IP   || process.env.SDEMO_SERVICE_HOST || '0.0.0.0';
+var port = 8080;
+var ip   = '0.0.0.0';
  
 app.listen(port, ip, function () {
   console.log( "Listening on " + ip + ", port " + port )
