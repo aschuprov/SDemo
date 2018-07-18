4c7b76efd529f70c4e0b3053a381098e2d8bd9d5
unknown <aschuprov@gmail.com>
2018-07-19 01:05:26 +0300

    env

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || process.env.SDEMO_SERVICE_PORT || 8080;
+var ip   = process.env.IP   || process.env.SDEMO_SERVICE_HOST || '0.0.0.0';
  
 app.listen(port, ip, function () {
   console.log( "Listening on " + ip + ", port " + port )
