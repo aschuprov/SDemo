90ffb08a408cce3cb80180ff106a7ec126fa184f
unknown <aschuprov@gmail.com>
2018-07-19 23:32:52 +0300

    Hello

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.send('Hello!');
 });
 
 var port = 8080;
