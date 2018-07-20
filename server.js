09ce4b68fae5b5af080929e855d74d7899de75dd
unknown <aschuprov@gmail.com>
2018-07-21 01:04:56 +0300

    ocr

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var ocrSDK = require('./ocrsdk.js');
+var request = require('request');
 
 var app = express();
 var router = express.Router(); 
@@ -8,6 +10,7 @@ app.use(express.static('public'));
 
 router.post('/', function (req, res) {
   var image = req.body;
+  ocr(image);
   res.json( { firstName: 'Alex', lastName: 'Chuprov' } );
 });
 
@@ -24,3 +27,56 @@ var ip   = '0.0.0.0';
 app.listen(port, ip, function () {
   console.log( "Listening on " + ip + ", port " + port )
 });
+
+function ocr(image) {
+  var ocrAPI = ocrSDK.create('SDemoOCR', 'Up0X00Yw8Eo1MzGRPQa1vvZt');
+  ocrAPI.serverUrl = 'http://cloud.ocrsdk.com';
+  var settings = new ocrSDK.ProcessingSettings();
+  settings.language = "Russian";
+  settings.exportFormat = "xml";
+  settings.profile = "textExtraction";
+  
+  console.log("Calling processImage");
+  ocrAPI.processImage(image, settings, function(error, taskData) {
+    if (error) {
+			console.log("Error: " + error.message);
+			return;
+    }
+    
+		console.log("Task id = " + taskData.id + ", status is " + taskData.status);
+		if (!ocrsdk.isTaskActive(taskData)) {
+			console.log("Unexpected task status " + taskData.status);
+			return;
+		}
+
+		ocrsdk.waitForCompletion(taskData.id, processingCompleted);
+  });
+
+  function processingCompleted(error, taskData) {
+		if (error) {
+			console.log("Error: " + error.message);
+			return;
+		}
+
+		if (taskData.status != 'Completed') {
+			console.log("Error processing the task.");
+			if (taskData.error) {
+				console.log("Message: " + taskData.error);
+			}
+			return;
+		}
+
+		console.log("Processing completed.");
+
+    var res = ocrsdk.getResult(taskData.resultUrl.toString(), downloadCompleted);
+    console.log("res=" + res);
+  }
+  
+  function downloadCompleted(error) {
+		if (error) {
+			console.log("Error: " + error.message);
+			return;
+		}
+		console.log("Done.");
+	}
+}
