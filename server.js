fd6a1aea8cc96eb4c20cf8c11d962c36cb4949f7
unknown <aschuprov@gmail.com>
2018-07-20 22:50:34 +0300

    Post

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
 });
 
 var port = 8080;
