b3b169e7c21c6723094e1354760f9e7a8f4b5383
Alex Chuprov <aschuprov@gmail.com>
2018-08-10 00:20:16 +0300

    decode

--- server.js
+++ server.js
@@ -82,18 +82,19 @@ function ocrSendFile(image) {
 		console.log("Done. Received=" + data); 
 	}
 
-	function decodeBase64Image(dataString) {
-		var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-			response = {};
-	
-		if (matches.length !== 3) {
-			return new Error('Invalid input string');
-		}
-	
-		response.type = matches[1];
-		response.data = new Buffer(matches[2], 'base64');
-	
-		return response;
-	}
 	*/
+}
+
+function decodeBase64Image(dataString) {
+	var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+		response = {};
+
+	if (matches.length !== 3) {
+		return new Error('Invalid input string');
+	}
+
+	response.type = matches[1];
+	response.data = new Buffer(matches[2], 'base64');
+
+	return response;
 }
